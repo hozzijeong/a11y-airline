777b5bb78228202fce6bfac8a807703dc9bfda79
정호진 <50974359+hozzijeong@users.noreply.github.com>
2023-07-28 12:30:58 +0900

    승객 관련 기능 웹 접근성 구현 (#2)
    * initial commit
    
    * chore: gh-pages 설치 및 추가
    
    * refactor: 숫자 변경시 증감 나타내는 기능 구현
    
    * reactor: 인원/감소에 따른 이름 변경

--- src/components/SpinButton.tsx
+++ src/components/SpinButton.tsx
@@ -1,55 +1,90 @@
-import React, { useState, MouseEvent } from "react";
-import "./SpinButton.css";
+import React, { useState, MouseEvent } from 'react';
+import './SpinButton.css';
 
 const SpinButton: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-  const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
+	const [count, setCount] = useState<number>(0);
+	const [isTooltipVisible, setIsTooltipVisible] = useState<boolean>(false);
 
-  const increment = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+	const [message, setMessage] = useState('');
 
-  const decrement = () => {
-    setCount((prevCount) => prevCount - 1);
-  };
+	const increment = () => {
+		if (count === 3) {
+			setMessage('최대 인원수는 3명까지만 가능합니다');
+			return;
+		}
+		setCount((prevCount) => prevCount + 1);
+		setMessage(`성인 승객 추가 ${count + 1} `);
+	};
 
-  const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
-    setIsTooltipVisible(!isTooltipVisible);
-  };
+	const decrement = () => {
+		if (count === 0) {
+			setMessage('최소 인원이 0이하로 갈 수 없습니다.');
+			return;
+		}
+		setCount((prevCount) => prevCount - 1);
+		setMessage(`성인 승객 감소 ${count - 1} `);
+	};
 
-  return (
-    <section className="spinButtonContainer">
-      <div>
-        <h1>승객 선택</h1>
-        <div className="spinButtonLabel">
-          <label>성인</label>
-          <div
-            className="helpIcon"
-            onMouseEnter={toggleTooltip}
-            onMouseLeave={toggleTooltip}
-          >
-            ?
-            {isTooltipVisible && (
-              <span className="tooltip">최대 인원수는 3명까지 가능합니다</span>
-            )}
-          </div>
-        </div>
-        <button onClick={decrement} className="spinButton">
-          -
-        </button>
-        <input
-          type="text"
-          role="spinbutton"
-          readOnly
-          className="spinButtonInput"
-          value={count}
-        />
-        <button onClick={increment} className="spinButton">
-          +
-        </button>
-      </div>
-    </section>
-  );
+	const toggleTooltip = (event: MouseEvent<HTMLDivElement>) => {
+		setIsTooltipVisible(!isTooltipVisible);
+	};
+
+	return (
+		<section className='spinButtonContainer'>
+			<div>
+				<h1>승객 선택</h1>
+				<div className='spinButtonLabel'>
+					<label htmlFor='adult-tooltip'>성인</label>
+					<div
+						className='helpIcon'
+						onMouseEnter={toggleTooltip}
+						onMouseLeave={toggleTooltip}
+						role='tooltip'
+						aria-controls='adult-tooltip'
+					>
+						?
+						{isTooltipVisible && (
+							<span id='adult-tooltip' role='alert' className='tooltip'>
+								최대 인원수는 3명까지 가능합니다
+							</span>
+						)}
+					</div>
+				</div>
+				<button
+					type='button'
+					aria-label='성인 탑승자 한명 줄이기 버튼'
+					onClick={decrement}
+					className='spinButton'
+				>
+					-
+				</button>
+				<input
+					type='text'
+					role='spinbutton'
+					readOnly
+					className='spinButtonInput'
+					value={count}
+					aria-label='성인 승객 인원'
+				/>
+				<button
+					type='button'
+					aria-label='성인 탑승자 한명 늘리기 버튼'
+					onClick={increment}
+					className='spinButton'
+				>
+					+
+				</button>
+				<div
+					role='alert'
+					aria-live='assertive'
+					aria-atomic={true}
+					className='hidden'
+				>
+					{message}
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SpinButton;
